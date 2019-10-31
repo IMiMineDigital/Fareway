f5bbea14b7fbba709b79846bfcfd509d422de851
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2019-10-31 18:25:29 +0530

    Android 6.6

--- app/src/main/java/com/fareway/activity/MainFwActivity.java
+++ app/src/main/java/com/fareway/activity/MainFwActivity.java
@@ -799,7 +799,8 @@ public class MainFwActivity extends AppCompatActivity
                         listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
             }
         } else {
-            //getMyLocation();
+            getMyLocation();
+            Toast.makeText(this, "Permission Already GRANTED", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -14084,7 +14085,7 @@ public class MainFwActivity extends AppCompatActivity
                 // progressDialog.setMessage("Processing");
                 //progressDialog.show();
 
-                StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, Constant.WEB_URL + Constant.LOGINSAVE + "&Information=" + appUtil.getPrefrence("Email") + "|" + appUtil.getPrefrence("Password") + "|" + deviceType + "|Android " + osName + "|" + myVersion + "|" + "" + "|" + "" + "|" + "" + "|" + Latitude + "|" + Longitude + "|6.5",
+                StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, Constant.WEB_URL + Constant.LOGINSAVE + "&Information=" + appUtil.getPrefrence("Email") + "|" + appUtil.getPrefrence("Password") + "|" + deviceType + "|Android " + osName + "|" + myVersion + "|" + "" + "|" + "" + "|" + "" + "|" + Latitude + "|" + Longitude + "|6.6",
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
