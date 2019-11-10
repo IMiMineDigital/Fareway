4f487ee9eb48327541de610e0901f4c05fededf2
anshuS006 <34213723+anshuS006@users.noreply.github.com>
2019-11-10 20:10:23 +0530

    Android 7.2

--- app/src/main/java/com/fareway/activity/MainFwActivity.java
+++ app/src/main/java/com/fareway/activity/MainFwActivity.java
@@ -14077,7 +14077,7 @@ public class MainFwActivity extends AppCompatActivity
                 // progressDialog.setMessage("Processing");
                 //progressDialog.show();
 
-                StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, Constant.WEB_URL + Constant.LOGINSAVE + "&Information=" + appUtil.getPrefrence("Email") + "|" + appUtil.getPrefrence("Password") + "|" + deviceType + "|Android " + osName + "|" + myVersion + "|" + "" + "|" + "" + "|" + "" + "|" + Latitude + "|" + Longitude + "|7.1",
+                StringRequest jsonObjectRequest = new StringRequest(Request.Method.POST, Constant.WEB_URL + Constant.LOGINSAVE + "&Information=" + appUtil.getPrefrence("Email") + "|" + appUtil.getPrefrence("Password") + "|" + deviceType + "|Android " + osName + "|" + myVersion + "|" + "" + "|" + "" + "|" + "" + "|" + Latitude + "|" + Longitude + "|7.2",
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
